704fe9be3d312629c7c585aed8ee6f39a4b5ef7d
Emily <126881207+hvemily@users.noreply.github.com>
2024-04-16 21:35:43 +0200

    ud

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-//* https://v2.api.noroff.dev/gamehub/ *\\
+const baseURL = "https://v1.api.noroff.dev/gamehub/"
 
-https://v2.api.noroff.dev/gamehub
+const getGames = document.querySelector(".games")
+
+async function getGames(){
+  
+  const req = await fetch(baseURL)
+
+  const result = await req.json()
+
+  console.log("game object", result)
+
+  const games = result.filter((games) => games.genre === "all")
+
+  for(let i = 0; i < games.length; i++)
+
+  getGames.innerHTML += `
+    <a href="./inde
+}
