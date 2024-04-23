1a7d713aabb86a7c3ae80840b3fd456de066a3db
Emily <126881207+hvemily@users.noreply.github.com>
2024-04-23 22:58:37 +0200

    losUpdados

--- js/script.js
+++ js/script.js
@@ -14,8 +14,12 @@ import { getGames } from "./fetch-api.js"
       gamesContainer.innerHTML = ""
     
       for (let i = 0; i < gamesList.length; i++) {
-        gamesContainer.innerHTML += `<div class="game">  
+        gamesContainer.innerHTML += `<div class="games">  
           <img src="${gamesList[i].image}" alt="">
+          <p>${gamesList[i].title}</p>
+          <p>${gamesList[i].price}</p>
+          <p>${gamesList[i].discountedPrice}</p>
+          
         </div>`
       }
     
@@ -24,34 +28,47 @@ import { getGames } from "./fetch-api.js"
 
     }
 
+    console.log(gamesList);
+
+// filtering by genre
+
+function filterByGenre(event) {
+
+  let selectedGenre = event.target.value 
+
+  if (selectedGenre === "All"){
+
+    getAPI.innerHTML = ""
+
+      for(let i = 0; i <gamesList.length; i++){
+        getAPI.innerHTML += `<a class="item-card" href="/product/index.html?arrid=${gameList[i].id}">                
+        <img src="${gameList[i].image}" alt="${gameList[i].title}"/>
+        <h3 class="title">${gameList[i].title}</h3>
+        <div class="flex-sale">
+          <p class="${gameList[i].onSale ? "on-sale" : ""}">${gameList[i].onSale ? gameList[i].price : ""}</p>
+          <p class="current-price">$ ${gameList[i].onSale ? gameList[i].discountedPrice : gameList[i].price} </p>
+        </div>
+      </a>` 
+      }
+      return
+    }
+
+  let filteredList = gamesList.filter(game => game.genre.toLowerCase() === selectedGenre.toLowerCase());
+
+  console.log("new filter list to sort by: ", filteredList);
 
 
-      // errorContainer.innerHTML += `<div class="error">
-      //               <h1>${allGames.msg}</h1>
-      //               <p>Error status: ${allGames.status}</p>
-      //               <p>Something went wrong</p>
-    
-      //   </div>`;
-    
- 
+  getAPI.innerHTML = ""
 
-      // try{
-      //   const response = await fetch(baseURL)
+  for (let i = 0; i < filteredList.length; i++){  // we use the previous variable of 'filteredList.length' that it has generated from before.
+     getAPI.innerHTML += `<a class="item-card" href="/product/index.html?arrid=${filteredList[i].id}">                
+      <img src="${filteredList[i].image}" alt="${filteredList[i].title}"/>
+      <h3 class="title">${filteredList[i].title}</h3>
+      <div class="flex-sale">
+      <p class="${filteredList[i].onSale ? "on-sale" : ""}">${filteredList[i].onSale ? filteredList[i].price : ""}</p>
+      <p class="current-price">$ ${filteredList[i].onSale ? filteredList[i].discountedPrice : filteredList[i].price} </p>
+                           </div>
+                         </a>`
+   };
+ };
 
-      //   if(!response.ok){
-      //       throw new Error("Could not fetch resource");
-      //   }
-
-      //   const gamesList = await response.json();
-      //   console.log(gamesList);
-      //   for(let i = 0;i<gamesList.length;i++){
-      //     gamesContainer.innerHTML += `<a href="product-list/index.html">
-      //     <img src="${gamesList[i].image.url}" alt="">
-      //     </a>`
-      //     }
-      // }
-      // catch(error){
-      //   console.error(error);
-      // }
-
-    
